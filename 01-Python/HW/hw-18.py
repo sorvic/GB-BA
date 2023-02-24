b03569d521f86986145d549899ed8f6f88eb1963
Sorvic_mbp-st <sorvic@gmail.com>
2023-02-24 20:51:07 +0300

    upd hw-03

--- 01-Python/HW/hw-18.py
+++ 01-Python/HW/hw-18.py
@@ -21,3 +21,60 @@ for i in set(list_1):
         res = i
 
 print(res)
+
+""" teacher's comment
+если list_1 = [3, 7, 9]
+Введите искомое число: 6
+3
+или 7 всё-таки ближе?
+Посмотри разбор домашних задач в записи 4-го семинара
+"""
+from random import randint
+n = int(input('Введите кол-во элементов: '))
+list_2 = [randint(1, n) for i in range(n)]
+print(list_2)
+# numbers = {1, 2, 3, 5, 9, 12}
+numbers = set(list_2)
+# num = 7
+num = int(input('Введите искомое число: '))
+dif = 0
+# чтобы не бегать по условиям - добавим сразу проервку,
+# что если число больше max - берем сразу max
+# что если число меньше min - берем сразу min
+if num > max(numbers):
+    print(max(numbers))
+elif num < min(numbers):
+    print(min(numbers))
+else:
+    while True:
+        if num - dif in numbers and num + dif in numbers and num - dif != num + dif:
+            print(num - dif, num + dif)
+            break
+        elif num - dif in numbers:
+            print(num - dif)
+            break
+        elif num + dif in numbers:
+            print(num + dif)
+            break
+        else:
+            dif += 1
+
+# -----------------------------
+""" IrinaZurina code """
+from random import randint
+
+size = int(input('Введите размер списка: '))
+numbers = tuple(randint(1, size) for _ in range(size))
+print(*numbers)
+num_X = int(input('Введите искомое число: '))
+mod = 0
+flag = False
+for _ in range(len(set(numbers))):
+    for i in set(numbers):
+        if i == num_X - mod or i == num_X + mod:
+            print(i)
+            flag = True
+            break
+    if flag:
+        break
+    mod += 1
--- 01-Python/HW/hw-20.py
+++ 01-Python/HW/hw-20.py
@@ -44,3 +44,27 @@ for i in word.upper():
     res += scrubble[i]
 
 print(res)
+
+
+
+# -----------------------------
+""" IrinaZurina code """
+# var1
+sum_score = sum([scrubble[i] for i in input().upper()])
+print(sum_score)
+
+# var2
+scores = {1: {'A', 'E', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'},
+          2: {'D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'},
+          3: {'B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'},
+          4: {'F', 'H', 'V', 'W', 'Y', 'Й', 'Ы'},
+          5: {'K', 'Ж', 'З', 'Х', 'Ц', 'Ч'},
+          8: {'J', 'X', 'Ш', 'Э', 'Ю'},
+          10: {'Q', 'Z', 'Ф', 'Щ', 'Ъ'}}
+sum_score = 0
+for letter in input('Введите слово: ').upper():
+    for key in scores.keys():
+        if letter in scores[key]:
+            sum_score += key
+            break
+print(sum_score)
